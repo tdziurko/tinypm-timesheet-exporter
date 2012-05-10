8175b40ddc4d3b707706374892e0509929038393
Tomasz Dziurko <tdziurko@gmail.com>
2012-05-10 16:21:27 +0200

    - Added method to load iterations for project - Fixed date format for iteration start date

--- tinypm-rest/src/main/java/com/google/code/tinypmclient/Iteration.java
+++ tinypm-rest/src/main/java/com/google/code/tinypmclient/Iteration.java
@@ -1,89 +1,88 @@
 package com.google.code.tinypmclient;
 
-import static javax.xml.bind.annotation.XmlAccessType.FIELD;
-
-import java.util.Date;
+import com.google.code.tinypmclient.internal.ActiveResource;
+import com.google.code.tinypmclient.internal.binding.DateAdapter;
 
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.Date;
 
-import com.google.code.tinypmclient.internal.ActiveResource;
-import com.google.code.tinypmclient.internal.binding.LongFormatDateAdapter;
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
 
 @XmlRootElement
 @XmlAccessorType(FIELD)
 public class Iteration extends ActiveResource {
 
-	private int id;
+    private int id;
 
-	private String name;
+    private String name;
 
-	private String goal;
+    private String goal;
 
-	private float plannedVelocity;
+    private float plannedVelocity;
 
-	private int position;
+    private int position;
 
-	@XmlJavaTypeAdapter(LongFormatDateAdapter.class)
-	private Date startDate;
+    @XmlJavaTypeAdapter(DateAdapter.class)
+    private Date startDate;
 
-	private int duration;
+    private int duration;
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getGoal() {
-		return goal;
-	}
+    public String getGoal() {
+        return goal;
+    }
 
-	public void setGoal(String goal) {
-		this.goal = goal;
-	}
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
 
-	public float getPlannedVelocity() {
-		return plannedVelocity;
-	}
+    public float getPlannedVelocity() {
+        return plannedVelocity;
+    }
 
-	public void setPlannedVelocity(float plannedVelocity) {
-		this.plannedVelocity = plannedVelocity;
-	}
+    public void setPlannedVelocity(float plannedVelocity) {
+        this.plannedVelocity = plannedVelocity;
+    }
 
-	public int getPosition() {
-		return position;
-	}
+    public int getPosition() {
+        return position;
+    }
 
-	public void setPosition(int position) {
-		this.position = position;
-	}
+    public void setPosition(int position) {
+        this.position = position;
+    }
 
-	public Date getStartDate() {
-		return startDate;
-	}
+    public Date getStartDate() {
+        return startDate;
+    }
 
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
 
-	public int getDuration() {
-		return duration;
-	}
+    public int getDuration() {
+        return duration;
+    }
 
-	public void setDuration(int duration) {
-		this.duration = duration;
-	}
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 
 }
