48bad4f832c808676d500cd754619fc452717f88
Tomasz Dziurko <tdziurko@gmail.com>
2012-05-13 08:53:12 +0200

    Output enum with converter

---
+++ timesheet-exporter/src/main/java/pl/softwaremill/timesheet_exporter/OutputConverter.java
@@ -0,0 +1,18 @@
+package pl.softwaremill.timesheet_exporter;
+
+import com.beust.jcommander.IStringConverter;
+import com.beust.jcommander.ParameterException;
+
+public class OutputConverter implements IStringConverter<OutputEnum> {
+
+    @Override
+    public OutputEnum convert(String value) {
+        OutputEnum convertedValue = OutputEnum.fromString(value);
+
+        if (convertedValue == null) {
+            throw new ParameterException("Value " + value + "can not be converted to OutputEnum. " +
+                    "Available values are: console, pdf, xls.");
+        }
+        return convertedValue;
+    }
+}
---
+++ timesheet-exporter/src/main/java/pl/softwaremill/timesheet_exporter/OutputEnum.java
@@ -0,0 +1,19 @@
+package pl.softwaremill.timesheet_exporter;
+
+public enum OutputEnum {
+
+    CONSOLE,
+    PDF,
+    XLS;
+
+    public static OutputEnum fromString(String code) {
+
+        for (OutputEnum output : OutputEnum.values()) {
+            if (output.toString().equalsIgnoreCase(code)) {
+                return output;
+            }
+        }
+
+        return null;
+    }
+}
